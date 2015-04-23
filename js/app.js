54f1712494ae9fc370f2f8ec674ac3258673c4fa
Choon-Chern Lim <limc@mayo.edu>
2015-04-23 13:40:49 -0500

    Wired formula to page

--- js/app.js
+++ js/app.js
@@ -12,82 +12,73 @@ var app = function () {
     var $userProgress = $( '.user-progress' );
     var $userScreen = $( '.user-screen' );
 
-    var $form = $( '#form' );
+    var $form = $( '.form' );
     var $userRiskText = $( '#user-risk-text' );
     var $userRiskRadio = $( 'input:radio[name=user-risk-radio]' );
+    var $usFnaRadio = $( 'input:radio[name=us-fna-radio]' );
 
     var $computedDiagram = $( '#computed-diagram' );
     var $computedValue = $( '#computed-value' );
 
-    // user selected risk, whether it is from user input or selected from the radio buttons
+    var malignancyCountText = $( '#malignancy-count-text' );
+    var nonMalignancyCountText = $( '#non-malignancy-count-text' );
+
     var userRiskPercentage = null;
+    var usFnaString = null;
 
     // formulas based on Naykky's Excel data
     var calculator = function () {
-        var FNA_RESULT = {
-            nonDiagnostic                   : {
-                estimate  : 0.515,
-                lower97ci : 0.336,
-                upper95ci : 0.791
-            },
-            benign                          : {
-                estimate  : 0.093,
-                lower97ci : 0.063,
-                upper95ci : 0.139
-            },
-            aus                             : {
-                estimate  : 0.405,
-                lower97ci : 0.228,
-                upper95ci : 0.719
-            },
-            suspiciousForFollicularNeoplasm : {
-                estimate  : 0.608,
-                lower97ci : 0.365,
-                upper95ci : 1.01
-            },
-            suspiciousForMalignancy         : {
-                estimate  : 8.35,
-                lower97ci : 3.63,
-                upper95ci : 19.23
-            },
-            malignant                       : {
-                estimate  : 196.77,
-                lower97ci : 67.99,
-                upper95ci : 569.51
-            }
+        var FNA_RESULT = [];
+        FNA_RESULT['nonDiagnostic'] = {
+            estimate  : 0.515,
+            lower97ci : 0.336,
+            upper95ci : 0.791
         };
-
-        // compute one FNA result
-        var compute = function ( percent, fnaResult ) {
-            var preTestOdds = percent / (1 - percent);
-            var postTestOddsEstimate = preTestOdds * fnaResult.estimate;
-            var postTestProbabilityEstimate = postTestOddsEstimate / (1 + postTestOddsEstimate) * 100;
-            var postTestOddsLower = preTestOdds * fnaResult.lower97ci;
-            var postTestProbabilityLower = postTestOddsLower / (1 + postTestOddsLower) * 100;
-            var postTestOddsHigher = preTestOdds * fnaResult.upper95ci;
-            var postTestProbabilityHigher = postTestOddsHigher / (1 + postTestOddsHigher) * 100;
-
-            return {
-                preTestOdds                 : preTestOdds,
-                postTestOddsEstimate        : postTestOddsEstimate,
-                postTestProbabilityEstimate : postTestProbabilityEstimate,
-                postTestOddsLower           : postTestOddsLower,
-                postTestProbabilityLower    : postTestProbabilityLower,
-                postTestOddsHigher          : postTestOddsHigher,
-                postTestProbabilityHigher   : postTestProbabilityHigher
-            };
+        FNA_RESULT['benign'] = {
+            estimate  : 0.093,
+            lower97ci : 0.063,
+            upper95ci : 0.139
+        };
+        FNA_RESULT['aus'] = {
+            estimate  : 0.405,
+            lower97ci : 0.228,
+            upper95ci : 0.719
+        };
+        FNA_RESULT['suspiciousForFollicularNeoplasm'] = {
+            estimate  : 0.608,
+            lower97ci : 0.365,
+            upper95ci : 1.01
+        };
+        FNA_RESULT['suspiciousForMalignancy'] = {
+            estimate  : 8.35,
+            lower97ci : 3.63,
+            upper95ci : 19.23
+        };
+        FNA_RESULT['malignant'] = {
+            estimate  : 196.77,
+            lower97ci : 67.99,
+            upper95ci : 569.51
         };
 
         return {
-            // compute all FNA results
-            computeAll : function ( percent ) {
+            compute : function ( percent, fnaResultString ) {
+                var fnaResult = FNA_RESULT[fnaResultString];
+                var preTestOdds = percent / (1 - percent);
+                var postTestOddsEstimate = preTestOdds * fnaResult.estimate;
+                var postTestProbabilityEstimate = postTestOddsEstimate / (1 + postTestOddsEstimate) * 100;
+                var postTestOddsLower = preTestOdds * fnaResult.lower97ci;
+                var postTestProbabilityLower = postTestOddsLower / (1 + postTestOddsLower) * 100;
+                var postTestOddsHigher = preTestOdds * fnaResult.upper95ci;
+                var postTestProbabilityHigher = postTestOddsHigher / (1 + postTestOddsHigher) * 100;
+
                 return {
-                    nonDiagnostic                   : compute( percent, FNA_RESULT.nonDiagnostic ),
-                    benign                          : compute( percent, FNA_RESULT.benign ),
-                    aus                             : compute( percent, FNA_RESULT.aus ),
-                    suspiciousForFollicularNeoplasm : compute( percent, FNA_RESULT.suspiciousForFollicularNeoplasm ),
-                    suspiciousForMalignancy         : compute( percent, FNA_RESULT.suspiciousForMalignancy ),
-                    malignant                       : compute( percent, FNA_RESULT.malignant )
+                    preTestOdds                 : preTestOdds,
+                    postTestOddsEstimate        : postTestOddsEstimate,
+                    postTestProbabilityEstimate : postTestProbabilityEstimate,
+                    postTestOddsLower           : postTestOddsLower,
+                    postTestProbabilityLower    : postTestProbabilityLower,
+                    postTestOddsHigher          : postTestOddsHigher,
+                    postTestProbabilityHigher   : postTestProbabilityHigher
                 };
             }
         }
@@ -157,39 +148,46 @@ var app = function () {
     };
 
     var setupScreen2 = function () {
+        $usFnaRadio.change( function () {
+            usFnaString = $( this ).val();
+        } );
     };
 
     var setupScreen3 = function () {
         var currentCount = 0;
         var color = '';
 
-        // TODO change this
-        var estimate = 43;
-        //var scores = calculator.computeAll( userRiskPercentage );
+        var result = calculator.compute( userRiskPercentage, usFnaString );
+
+        var malignancyCount = Math.round( result.postTestProbabilityEstimate );
+        var nonMalignancyCount = 100 - malignancyCount;
+
+        var lower = Math.round( result.postTestProbabilityLower );
+        var higher = Math.round( result.postTestProbabilityHigher );
 
         // display diagram
         $computedDiagram.empty();
 
         for ( var row = 0; row < 10; ++row ) {
-            var $row = $( '<div class="row">' );
-
-            $row.append( '<div class="col s1">&nbsp;</div>' );
+            var $row = $( '<span style="line-height: 1">' );
 
             for ( var col = 0; col < 10; ++col ) {
-                color = currentCount++ < estimate ? "teal" : "orange";
-                $row.append( '<div class="col s1"><i class="btn-small-circle btn-floating disabled ' +
-                             color +
-                             '"></i></div>' );
+                color = currentCount++ < malignancyCount ? "red-text" : "teal-text";
+                $row.append( '<i class="small mdi-social-person ' + color + '"></i>' );
             }
 
-            $row.append( '<div class="col s1">&nbsp;</div>' );
+            $row.append( '<br/>' );
 
             $computedDiagram.append( $row );
         }
 
-        // display computed score
-        // TODO fix this!
-        $computedValue.text( estimate + ' % ( 40 – 70 )' );
+        malignancyCountText.text( malignancyCount + ' patient' +
+                                  (malignancyCount != 1 ? 's' : '') + ' with malignancy' );
+
+        nonMalignancyCountText.text( nonMalignancyCount + ' patient' +
+                                     (nonMalignancyCount != 1 ? 's' : '') + ' without malignancy' );
+
+        $computedValue.text( malignancyCount + ' % ( ' + lower + ' – ' + higher + ' )' );
     };
 
     return {
@@ -211,9 +209,13 @@ var app = function () {
                         errorMessage = 'Please specify a value for risk probability first.'
                     }
                 }
-                // TODO check input from screen 2 first
                 else if ( currentScreen === 2 ) {
-                    setupScreen3();
+                    if ( usFnaString ) {
+                        setupScreen3();
+                    }
+                    else {
+                        errorMessage = 'Please enter US-FNA result first.'
+                    }
                 }
 
                 if ( errorMessage ) {
@@ -231,6 +233,7 @@ var app = function () {
                 $form.trigger( 'reset' );
             } );
 
+            // setup and show first screen
             setupScreen1();
             displayScreen();
         }
